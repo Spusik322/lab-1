894e27e3b41382d2d49c2191ec87818fd3eb9b55
Spusik228 <vatkin502@gmail.com>
2024-09-15 21:57:44 +0300

    LABA 1

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+long long int factorial(int i)
+{
+  if (i==0) 
+    return 1;
+  else 
+    return i*factorial(i-1);
+}
+long double cosx(long double x, long double eps)
+{
+    long double res = 1;
+    long double frac;
+    int zn = -1;
+    int mn = 2;
+    do{
+        frac = (pow(x,mn))/(factorial(mn));
+        cout << frac*zn << " "; 
+        mn += 2;
+        res = res + zn*frac;
+        zn *= -1;     
+    }while(frac > eps);
+    return res;
+}
+int main(){
+long double x;
+int k;
+cout << "Введите x = ";
+cin >> x;
+cout << endl << "Введите k = ";
+cin >> k;
+long double eps = pow(10, (-k));
+cout << endl << cosx(x , eps);
+}
