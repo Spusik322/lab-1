21205bad892f0a6fe25622671b7adbbec1f7169c
Tihon Vatkin <fpm.vatkin@bsu.by>
2024-10-02 13:59:42 +0300

    ГОТОВО

--- main.cpp
+++ main.cpp
@@ -1,27 +1,18 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-long long int factorial(int i)
-{
-  if (i==0) 
-    return 1;
-  else 
-    return i*factorial(i-1);
-}
 long double cosx(long double x, long double eps)
 {
     long double res = 1;
     long double frac = 1;
     int zn = -1;
     int mn = 2;
-    long double frac1 = 1;
     do{
-        frac = frac*x*x;
-        frac1 = frac/factorial(mn);
+        frac = (frac)*((x*x)/(mn*(mn-1)));
         mn += 2;
-        res = res + zn*frac1;
+        res = res + zn*frac;
         zn *= -1;     
-    }while(frac1 > eps);
+    }while(frac > eps);
     return res;
 }
 int main(){
